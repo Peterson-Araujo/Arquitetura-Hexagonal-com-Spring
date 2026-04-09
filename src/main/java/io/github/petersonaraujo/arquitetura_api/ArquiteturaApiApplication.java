4b96cd2ed77ab21f2ee28e6b5fa2043125c9fa77
Peterson Araujo <petersonaraujo70@gmail.com>
2026-04-09 08:19:46 -0300

    feat: Criação do projeto

---
+++ src/main/java/io/github/petersonaraujo/arquitetura_api/ArquiteturaApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.petersonaraujo.arquitetura_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/petersonaraujo/arquitetura_api/ArquiteturaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.petersonaraujo.arquitetura_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
